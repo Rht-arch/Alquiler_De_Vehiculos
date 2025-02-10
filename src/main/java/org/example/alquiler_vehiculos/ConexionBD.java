6bc8afe249b3bc115a14097a9f910f77867d545e
alejandroparodcristian <115542447+alejandroparodcristian@users.noreply.github.com>
2025-02-10 17:43:23 +0100

    ConexionBD

---
+++ src/main/java/org/example/alquiler_vehiculos/ConexionBD.java
@@ -0,0 +1,21 @@
+package org.example.alquiler_vehiculos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    private static final String URL = "jdbc:mysql://localhost:3306/alquiler_vehiculos_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar con la base de datos");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
