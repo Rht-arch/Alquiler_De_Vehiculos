54ee98e57ef0e97ef0eb76a7479df1e446985f81
AliciaPM08 <apachecomena@gmail.com>
2025-02-10 09:51:52 +0100

    Cambio del titulo

--- src/main/java/org/example/alquiler_vehiculos/HelloApplication.java
+++ src/main/java/org/example/alquiler_vehiculos/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("InicioDeSesion.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 350);
-        stage.setTitle("Hello!");
+        stage.setTitle("Alquiler de coches");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
