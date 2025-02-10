178de767e4845761cdba1fa52317977b0d64148f
Rafael Haro <rafaharotello2003@gmail.com>
2025-02-10 09:45:15 +0100

    Modificación general

--- src/main/java/org/example/alquiler_vehiculos/HelloApplication.java
+++ src/main/java/org/example/alquiler_vehiculos/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("admin.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("InicioDeSesion.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 650, 350);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
