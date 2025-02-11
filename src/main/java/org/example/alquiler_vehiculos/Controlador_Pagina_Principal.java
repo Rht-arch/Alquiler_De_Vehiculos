183630fe5013a5c8bff6b29a727863edb73bda21
alejandroparodcristian <115542447+alejandroparodcristian@users.noreply.github.com>
2025-02-11 20:23:37 +0100

    Cambio para devolver Vehiculos por filtros

--- src/main/java/org/example/alquiler_vehiculos/Controlador_Pagina_Principal.java
+++ src/main/java/org/example/alquiler_vehiculos/Controlador_Pagina_Principal.java
@@ -124,5 +124,9 @@ public class Controlador_Pagina_Principal {
                 }
             }
         }
+//        VehiculoDAO vehiculoDAO = new VehiculoDAO();
+//        tablaVehiculos.setItems(FXCollections.observableArrayList(
+//                vehiculoDAO.vehiculoFiltros(marcaSeleccionada, tipoSeleccionado, modeloSeleccionada)
+//        ));
     }
 }
