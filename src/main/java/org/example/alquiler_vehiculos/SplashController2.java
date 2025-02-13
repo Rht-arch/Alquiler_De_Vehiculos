2e842760ad7787d81796d5dcaa9f4d85d21e9de8
AliciaPM08 <apachecomena@gmail.com>
2025-02-13 10:17:45 +0100

    Docu de los Splash

--- src/main/java/org/example/alquiler_vehiculos/SplashController2.java
+++ src/main/java/org/example/alquiler_vehiculos/SplashController2.java
@@ -8,14 +8,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
 
+/**
+ * Clase que controla la segunda pantalla del splash
+ * @author Alicia Pacheco Mena
+ */
 public class SplashController2 {
-    //Variables
+    /**
+     * ImageView que recoge el gif
+     */
     @FXML
     private ImageView gifImageView;
+    /**
+     * ProgressBar para cargar la barra de progreso
+     */
     @FXML
     private ProgressBar progressBar;
 
-    // Metodo para iniciar el splash con la barra de progreso
+
+    /**
+     * Metodo que se gestiona el tiempo de carga de la barra de progreso
+     * @param onComplete Devuelve que se ha completado el progreso de carga
+     */
     public void startSplash(Runnable onComplete) {
         try {
             String gifPath = getClass().getResource("/imagenes/splash2.gif").toExternalForm();
