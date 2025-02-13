faebe06f332859716c8c04684a3f35cfdbba3d7d
AliciaPM08 <apachecomena@gmail.com>
2025-02-13 10:24:58 +0100

    Docu del HelloApplication

--- src/main/java/org/example/alquiler_vehiculos/HelloApplication.java
+++ src/main/java/org/example/alquiler_vehiculos/HelloApplication.java
@@ -7,7 +7,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase que inicia toda la aplicacion
+ * @authors Alicia Pacheco, Rafael Haro y Cristian Alejandro
+ */
 public class HelloApplication extends Application {
+    /**
+     * Metodo que inicia toda la aplicacion
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException
+     */
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -19,6 +31,11 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+
+    /**
+     * Metodo que implementa el metodo start
+     * @param args
+     */
     public static void main(String[] args) {
         launch();
     }
