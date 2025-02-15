f30445ecb8557bab55fda17927910997964ccf3e
AliciaPM08 <apachecomena@gmail.com>
2025-02-15 21:39:27 +0100

    Idiomas compra

--- src/main/java/org/example/alquiler_vehiculos/Controlador_Compra.java
+++ src/main/java/org/example/alquiler_vehiculos/Controlador_Compra.java
@@ -1,22 +1,24 @@
 package org.example.alquiler_vehiculos;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.text.Text;
 import org.example.alquiler_vehiculos.BD.AlquilerDetalle;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Clase que controla la clase compra
  * @author Cristian Alejandro
  */
 public class Controlador_Compra {
-
+    @FXML
+    private Label txDetalles;
     /**
      * TableView para msotrar lso vehiculos
      */
@@ -40,6 +42,14 @@ public class Controlador_Compra {
      * Variable alquilar detalle
      */
     private AlquilerDetalle vehiculoSeleccionado;
+
+    @FXML
+    private ComboBox<String> comboBoxIdiomas;
+    /**
+     * Variables para establecer el idioma
+     */
+    private Locale locale;
+    private ResourceBundle bundle;
     /**
      * Lista que contiene todos los alquileres
      */
@@ -50,6 +60,28 @@ public class Controlador_Compra {
      */
     @FXML
     public void initialize() {
+        // Idioma por defecto
+        comboBoxIdiomas.getItems().addAll("Español", "English");
+        comboBoxIdiomas.getSelectionModel().select("Español");
+
+        // Cambio de idioma
+        comboBoxIdiomas.setOnAction(event -> {
+            String selectedLanguage = comboBoxIdiomas.getValue();
+
+            switch (selectedLanguage) {
+                case "English":
+                    Locale.setDefault(Locale.ENGLISH);
+                    locale = Locale.ENGLISH;
+                    break;
+                default:
+                    Locale.setDefault(new Locale("es", "ES"));
+                    locale = new Locale("es", "ES");
+                    break;
+            }
+
+            bundle = ResourceBundle.getBundle("org.example.alquiler_vehiculos.idioma", locale);
+            updateTexts();
+        });
         // Configurar las columnas de la tabla con las propiedades del objeto AlquilerDetalle
         colMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
         colModelo.setCellValueFactory(new PropertyValueFactory<>("modelo"));
@@ -61,6 +93,24 @@ public class Controlador_Compra {
 
         // Asignar la lista de alquileres a la tabla
         tablaCompra.setItems(listaAlquileres);
+    }
+
+    private void updateTexts() {
+        // Actualizar el título
+        txDetalles.setText(bundle.getString("label.detallesAlquiler"));
+
+        // Actualizar los textos de las columnas de la tabla
+        colMarca.setText(bundle.getString("col.marca"));
+        colModelo.setText(bundle.getString("col.modelo"));
+        colTipo.setText(bundle.getString("col.tipo"));
+        colIdCliente.setText(bundle.getString("col.idCliente"));
+        colFechaInicio.setText(bundle.getString("col.fechaInicio"));
+        colFechaFin.setText(bundle.getString("col.fechaFin"));
+        colTotal.setText(bundle.getString("col.total"));
+
+        // Actualizar los textos de los botones
+        btnComprar.setText(bundle.getString("button.alquilar"));
+        btnVolver.setText(bundle.getString("button.volver"));
     }
 
     /**
