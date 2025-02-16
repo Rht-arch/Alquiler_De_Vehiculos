0e85fd129de6b48a97c7506c123992cc537b6f59
rht-arch <rafaharotello2003@gmail.com>
2025-02-16 15:33:58 +0100

    JavaDoc

--- src/main/java/org/example/alquiler_vehiculos/SesionUsuario.java
+++ src/main/java/org/example/alquiler_vehiculos/SesionUsuario.java
@@ -1,11 +1,28 @@
 package org.example.alquiler_vehiculos;
 
+/**
+ * Clase que almacena el id del usuario
+ * @author Rafael Haro
+ */
 public class SesionUsuario {
+    /**
+     * Crea una instancia
+     */
     private static SesionUsuario instancia;
+    /**
+     * Almacena la id
+     */
     private int userId;
 
-    private SesionUsuario() { } // Constructor privado para Singleton
+    /**
+     * Contructor vacio
+     */
+    private SesionUsuario() { }
 
+    /**
+     * Metodo par obtener la instacia
+     * @return retorna la instancia
+     */
     public static SesionUsuario getInstancia() {
         if (instancia == null) {
             instancia = new SesionUsuario();
@@ -13,10 +30,18 @@ public class SesionUsuario {
         return instancia;
     }
 
+    /**
+     * Setter para la id
+     * @param userId Id del usuario
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * Getter del usuario
+     * @return la id el usuario
+     */
     public int getUserId() {
         return userId;
     }
